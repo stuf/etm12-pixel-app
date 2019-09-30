f9c544618b6e42c6ca00420ad282bdfca28a1f23
Stefan Rimaila <stefan@rimaila.fi>
2019-10-01 00:26:05 +0300

    Update palette to be editable

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -4,23 +4,23 @@ import * as R from 'ramda';
 
 import style from './Palette.module.scss';
 
-function Palette({ items }) {
+function Palette({ items, currentColor }) {
   return (
     <div className={style.root}>
       <ul className={style.items}>
         {U.thru(
           items,
-          U.mapValue(
-            R.map(it => (
-              <li key={it}>
+          U.mapElems((it, i) => {
+            const isActive = U.combine([i, currentColor], R.equals);
+            return (
+              <li key={`color-${i}`} className={U.when(isActive, style.active)}>
                 <button
-                  style={{
-                    backgroundColor: it,
-                  }}
-                ></button>
+                  style={{ backgroundColor: it }}
+                  onClick={U.doSet(currentColor, i)}
+                />
               </li>
-            )),
-          ),
+            );
+          }),
         )}
       </ul>
     </div>
