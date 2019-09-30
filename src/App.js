1d1749e273f699c2bdb693e1e6ad886801037f24
Stefan Rimaila <stefan@rimaila.fi>
2019-10-01 00:26:16 +0300

    Update base styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import * as React from 'karet';
 import * as U from 'karet.util';
-import * as R from 'ramda';
 
 import Canvas from './components/Canvas';
 import Palette from './components/Palette';
@@ -14,11 +13,15 @@ import styles from './App.module.scss';
  */
 function App({ state }) {
   const { size, scale } = U.thru(state, U.view('canvas'), U.destructure);
+  const { currentColor } = U.thru(state, U.view('color'), U.destructure);
 
   return (
     <main className={styles.root}>
       <div className={styles.left}>
-        <Palette items={U.view(['color', 'palettes', 0, 'items'], state)} />
+        <Palette
+          {...{ currentColor }}
+          items={U.view(['color', 'palettes', 0, 'items'], state)}
+        />
       </div>
       <div className="relative-pos">
         <PixelGrid {...{ size, scale }} />
