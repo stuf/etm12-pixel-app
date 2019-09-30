d020b122a705d515c52b801040730ec8165933fa
Stefan Rimaila <stefan@rimaila.fi>
2019-10-01 00:25:19 +0300

    Add separate state

---
+++ src/core/state.js
@@ -0,0 +1,27 @@
+import * as U from 'karet.util';
+// eslint-disable-next-line
+import * as K from 'kefir';
+
+const initialState = {
+  canvas: {
+    size: [32, 32],
+    scale: 16,
+  },
+  color: {
+    currentColor: 0,
+    currentPalette: 0,
+    palettes: [
+      {
+        name: 'bgb',
+        items: ['#081820', '#346856', '#88c070', '#e0f8d0'],
+      },
+    ],
+  },
+};
+
+/**
+ * @type {K.Property<typeof initialState, never>}
+ */
+const state = U.atom(initialState);
+
+export default state;
