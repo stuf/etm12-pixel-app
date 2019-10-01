a8908a19c9f225e92f1844d117695b488c76435c
Stefan Rimaila <stefan@rimaila.fi>
2019-10-01 19:15:49 +0300

    Add global typedefs

---
+++ src/global.d.ts
@@ -0,0 +1,42 @@
+declare global {
+  /**
+   * Interface for color palettes
+   */
+  interface IPalette {
+    /**
+     * Name of the palette
+     */
+    name: string;
+    /**
+     * Palette's colors as hexstrings
+     */
+    items: string[];
+  }
+
+  /**
+   * Interface for representing application state
+   */
+  interface IState {
+    canvas: {};
+    color: {
+      palettes: IPalette[];
+    };
+  }
+
+  //
+
+  /**
+   * Drawing canvas interface
+   */
+  interface ICanvas {
+    /**
+     * 2-tuple representing the (width, height) of the canvas
+     */
+    size: [number, number];
+
+    /**
+     * Scaling factor for the drawing canvas
+     */
+    scale: number;
+  }
+}
