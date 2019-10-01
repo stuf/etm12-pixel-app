571b25b6f9ae69df910e30e0a61cc80a69984bb0
Stefan Rimaila <stefan@rimaila.fi>
2019-10-01 20:09:59 +0300

    feat(canvas): stop drawing when mouse is moved outside of the canvas element

--- src/core/mouse.js
+++ src/core/mouse.js
@@ -23,10 +23,15 @@ const takeEvents = (type, source) =>
 export const mouseEventsFor = source => ({
   onButtonDown: takeEvents('mousedown', source),
   onButtonUp: takeEvents('mouseup', source),
+  onMouseOver: takeEvents('mouseover', source),
+  onMouseOut: takeEvents('mouseout', source),
   onMove: takeEvents('mousemove', source),
   onDrag: takeEvents('mousedown', source).flatMapLatest(() =>
     takeEvents('mousemove', source).takeUntilBy(
-      takeEvents('mouseup', source).take(1),
+      K.merge([
+        takeEvents('mouseup', source),
+        takeEvents('mouseout', source),
+      ]).take(1),
     ),
   ),
 });
@@ -42,6 +47,8 @@ export const mouseEventsFor = source => ({
  * @typedef {object} IMouseEvents
  * @prop {ObservableMouseEvent} onButtonDown
  * @prop {ObservableMouseEvent} onButtonUp
+ * @prop {ObservableMouseEvent} onMouseOver
+ * @prop {ObservableMouseEvent} onMouseOut
  * @prop {ObservableMouseEvent} onMove
  * @prop {ObservableMouseEvent} onDrag
  */
