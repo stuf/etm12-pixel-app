b74309c67be4fc2b69d7a8e2997e32b5e94f4a98
Stefan Rimaila <stefan@rimaila.fi>
2019-10-04 21:55:11 +0300

    test(shared): add basic tests for shared utils

--- src/shared.test.js
+++ src/shared.test.js
@@ -1,20 +1,23 @@
-describe('shared', () => {
-  it.todo('takeAll');
+import * as H from './shared';
+import * as T from './tests';
 
-  it.todo('reciprocal');
-  it.todo('reciprocalU');
-  it.todo('scaleSize');
+describe('shared', () => {
+  T.testEq([1, 2, 3], () => H.takeAll(1, 2, 3));
+
+  T.testEq(0.125, () => H.reciprocal(8));
+  T.testEq(0.125, () => H.reciprocalU(8));
+  T.testEq([16, 16], () => H.scaleSize([4, 4], 4));
 
   it.todo('layerPos');
   it.todo('screenPos');
   it.todo('pagePos');
 
-  it.todo('fromHex');
+  T.testEq([255, 0, 0, 255], () => H.fromHex('ff0000'));
 
   it.todo('getContext');
 
-  it.todo('fstOf');
-  it.todo('sndOf');
+  T.testEq('a', () => H.fstOf(['a', 'b']));
+  T.testEq('b', () => H.sndOf(['a', 'b']));
 
   it.todo('rangeScaled');
   it.todo('yiqFor');
