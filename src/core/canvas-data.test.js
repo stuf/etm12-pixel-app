bdf018b433ef97e5e1cc4358f22baad43782f418
Stefan Rimaila <stefan@rimaila.fi>
2019-10-04 21:51:39 +0300

    test(canvas-data): add initial tests

--- src/core/canvas-data.test.js
+++ src/core/canvas-data.test.js
@@ -1,10 +1,14 @@
 import canvasData from './canvas-data';
 
 describe('canvas-data', () => {
-  it.todo('clearImage_');
-  it.todo('clearImage');
-  it.todo('resizeImage_');
-  it.todo('resizeImage');
+  it.todo('data state');
+
+  describe('functions', () => {
+    it.todo('clearImage_');
+    it.todo('clearImage');
+    it.todo('resizeImage_');
+    it.todo('resizeImage');
+  });
 
   describe('undo support', () => {
     it.todo('collect history after modifying');
