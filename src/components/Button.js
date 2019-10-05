7bf31b6bf664bc5d721811568d4eda9e7b60edd4
Stefan Rimaila <stefan@rimaila.fi>
2019-10-05 12:30:47 +0300

    feat(components): add typedefs for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import * as React from 'karet';
 
+import * as T from './Button';
+
 import style from './Button.module.scss';
 
+/**
+ * @param {T.Props} props
+ */
 function Button({ children }) {
   return <button className={style.root}>{children}</button>;
 }
