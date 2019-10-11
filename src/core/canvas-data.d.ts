8a79a7b3ffc659d7657694ab51d5f310702993cc
Stefan Rimaila <stefan@rimaila.fi>
2019-10-11 14:23:25 +0300

    chore(core): improve typedefs for canvas data

--- src/core/canvas-data.d.ts
+++ src/core/canvas-data.d.ts
@@ -1,1 +1,3 @@
 export type CanvasData = number[];
+
+export type CanvasState = CanvasData;
--- src/core/canvas-data.js
+++ src/core/canvas-data.js
@@ -3,13 +3,12 @@
  * @module canvas-data
  * @namespace core
  */
-import * as K from 'kefir';
 import * as H from 'kefir.partial.lenses.history';
 import * as U from 'karet.util';
 import * as R from 'kefir.ramda';
 import * as L from 'kefir.partial.lenses';
 
-import { COLOR_CHANNELS } from '../constants';
+import * as S from '../settings';
 
 import * as T from './canvas-data.d';
 
@@ -20,35 +19,30 @@ import * as T from './canvas-data.d';
  */
 const initValue = [];
 
-const state = U.atom(H.init({ maxCount: 10, replacePeriod: 200 }, initValue));
+const state = U.atom(
+  H.init(
+    { maxCount: S.history.maxCount, replacePeriod: S.history.replacePeriod },
+    initValue,
+  ),
+);
 
 export default state;
 
 //
 
-/**
- * @return {void}
- * @deprecated
- */
 export const clearImage_ = () => U.view(L.elems, state).modify(R.always(0));
 
-/**
- * @return {typeof state}
- * @deprecated
- */
 export const clearImage = () => clearImage_() || state;
 
 /**
  * @param {number} w
  * @param {number} h
- * @return {void}
  */
 export const resizeImage_ = (w, h) =>
-  state.set(Array(w * h * COLOR_CHANNELS).fill(0));
+  state.set(Array(w * h * S.canvas.colorChannels).fill(0));
 
 /**
  * @param {number} w
  * @param {number} h
- * @return {typeof state}
  */
 export const resizeImage = (w, h) => resizeImage_(w, h) || state;
