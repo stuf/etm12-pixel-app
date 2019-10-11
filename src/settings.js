81c5e0b3686f9eb6a8c0e083a7ffc9ce5b07fb88
Stefan Rimaila <stefan@rimaila.fi>
2019-10-11 14:23:00 +0300

    feat(settings): add base settings file

---
+++ src/settings.js
@@ -0,0 +1,12 @@
+// @ts-check
+/**
+ * Settings related to undo/redo functinality
+ */
+export const history = {
+  maxCount: 10,
+  replacePeriod: 200,
+};
+
+export const canvas = {
+  colorChannels: 4,
+};
