066b65be55caa168e080449142eda0bbf7083fd5
Stefan Rimaila <stefan@rimaila.fi>
2019-10-11 15:10:08 +0300

    feat(components): add initial ColorStats component

---
+++ src/components/ui/ColorStats/index.js
@@ -0,0 +1,15 @@
+/* eslint no-unused-vars: [1, {"varsIgnorePattern": "[K|T]"}] */
+import * as React from 'karet';
+import * as R from 'kefir.ramda';
+
+import * as T from './index.d';
+import styles from './index.module.scss';
+
+/**
+ * @param {T.Props} props
+ */
+function ColorStats({ data }) {
+  return <div className={styles.root}>stats</div>;
+}
+
+export default ColorStats;
