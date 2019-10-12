d1ac10d033f550e66d46907bacc0fe9b6e88865f
Stefan Rimaila <stefan@rimaila.fi>
2019-10-12 17:27:28 +0300

    chore(core): add jsdoc to keyboard events

--- src/core/keyboard.js
+++ src/core/keyboard.js
@@ -1,4 +1,14 @@
+/* eslint no-unused-vars: [1, {"varsIgnorePattern": "[K|T]"}] */
+import * as K from 'kefir';
+
 import { takeEvents } from './_shared';
 
+/**
+ * @type {K.Property<KeyboardEvent, any>}
+ */
 export const onKeyDown = takeEvents('keydown', document);
+
+/**
+ * @type {K.Property<KeyboardEvent, any>}
+ */
 export const onKeyUp = takeEvents('keyup', document);
