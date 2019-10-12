dcfa75910515f316b6c7b1d3d5150aaed704177f
Stefan Rimaila <stefan@rimaila.fi>
2019-10-13 02:52:43 +0300

    feat(scene): add initial SplashScene

---
+++ src/scenes/Splash/index.js
@@ -0,0 +1,13 @@
+import * as React from 'karet';
+
+import * as T from './index.d';
+
+/**
+ * @param {T.Props} props
+ * @return {T.Component}
+ */
+function SplashScene(props) {
+  return <div />;
+}
+
+export default SplashScene;
