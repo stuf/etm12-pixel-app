6be3659ca15160327170afcbef435898be0bbda0
Stefan Rimaila <stefan@rimaila.fi>
2019-10-13 21:42:44 +0300

    fix: fix broken settings tests

--- src/settings.test.js
+++ src/settings.test.js
@@ -1,15 +1,17 @@
 import * as R from 'ramda';
 import * as V from 'partial.lenses.validation';
 
-import * as S from './settings';
+import * as S from 'settings';
 
 const isNonEmpty = R.identity;
 const isNumber = R.is(Number);
 const isBool = R.is(Boolean);
+const isDefined = R.isNil;
 
 //
 
 const Rule = {
+  DEFINED: [isDefined, 'required'],
   REQUIRED: [isNonEmpty, 'required'],
   NUMBER: [isNumber, 'non-number'],
   BOOLEAN: [isBool, 'non-boolean'],
@@ -18,16 +20,16 @@ const Rule = {
 //
 
 const validate = (rules, actual) =>
-  expect(V.errors(rules, actual)).not.toBeDefined();
+  expect(V.errors(rules, actual)).toBeUndefined();
 
 //
 
 it('has correct history configuration', () => {
   validate(
     V.props({
-      maxCount: V.and(Rule.REQUIRED, Rule.NUMBER),
-      pushEquals: V.and(Rule.REQUIRED, Rule.BOOLEAN),
-      replacePeriod: V.and(Rule.REQUIRED, Rule.NUMBER),
+      maxCount: V.optional(V.and(Rule.REQUIRED, Rule.NUMBER)),
+      pushEquals: V.optional(Rule.BOOLEAN),
+      replacePeriod: V.optional(V.and(Rule.REQUIRED, Rule.NUMBER)),
     }),
     S.history,
   );
