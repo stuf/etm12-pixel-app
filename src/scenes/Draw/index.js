3b7afca1c6853bb613164daa1e45a52632d254b6
Stefan Rimaila <stefan@rimaila.fi>
2019-10-13 03:08:24 +0300

    feat(scenes): add DrawScene

---
+++ src/scenes/Draw/index.js
@@ -0,0 +1,17 @@
+/**
+ * @module DrawScene
+ * @namespace scenes
+ */
+import * as React from 'karet';
+
+import * as T from './index.d';
+
+/**
+ * @param {T.Props} props
+ * @return {T.Component}
+ */
+function DrawScene(props) {
+  return <div>draw</div>;
+}
+
+export default DrawScene;
