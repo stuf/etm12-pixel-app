f5812b002462cf8367ae932d87ef6c053f1aceba
Stefan Rimaila <stefan@rimaila.fi>
2019-10-14 15:59:49 +0300

    chore: add build-specific env vars to typedefs

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,11 +2,18 @@ import { Observable, Property } from 'kefir';
 import { IState, ICanvasState } from 'core/state';
 import { IMenuItem } from 'core/models';
 
+export interface BuildCommitEnv {
+  REACT_APP_BUILD_BRANCH: string;
+  REACT_APP_BUILD_COMMIT: string;
+  REACT_APP_BUILD_ENV: string;
+  REACT_APP_BUILD_VERSION: string;
+}
+
 export interface RootProps {
   state: IState;
   canvasData: ICanvasState;
   menuItems: IMenuItem[];
-  env: { [key: string]: string };
+  env: BuildCommitEnv & { [key: string]: string };
 }
 
 export type Value<T> = Property<T, any>;
