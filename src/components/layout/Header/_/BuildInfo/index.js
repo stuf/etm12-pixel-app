bf233379606bb33b53fa2e8f3247384663edc337
Stefan Rimaila <stefan@rimaila.fi>
2019-10-14 22:10:13 +0300

    test(components): add snapshot testing to Header

--- src/components/layout/Header/_/BuildInfo/index.js
+++ src/components/layout/Header/_/BuildInfo/index.js
@@ -1,5 +1,7 @@
 /* eslint no-unused-vars: [1, {"varsIgnorePattern": "[K|T]"}] */
 import * as React from 'karet';
+import * as U from 'karet.util';
+import * as R from 'kefir.ramda';
 
 import * as T from './index.d';
 
@@ -7,13 +9,16 @@ import * as T from './index.d';
  * @param {T.Props} props
  * @return {T.Component}
  */
-function BuildInfo({ env }) {
+function BuildInfo({ env = {} }) {
   return (
     <div className="flex-horizontal -vertical-center -justify-end pad -p-right">
-      <div className="text -right -small">
-        {env.REACT_APP_BUILD_VERSION} ({env.REACT_APP_BUILD_COMMIT}) – env:{' '}
-        {env.REACT_APP_BUILD_ENV} – branch: {env.REACT_APP_BUILD_BRANCH}
-      </div>
+      {U.unless(
+        R.isEmpty(env),
+        <div className="text -right -small">
+          {env.REACT_APP_BUILD_VERSION} ({env.REACT_APP_BUILD_COMMIT}) – env:{' '}
+          {env.REACT_APP_BUILD_ENV} – branch: {env.REACT_APP_BUILD_BRANCH}
+        </div>,
+      )}
     </div>
   );
 }
---
+++ src/components/layout/Header/_/BuildInfo/index.test.js
@@ -0,0 +1,22 @@
+import * as React from 'karet';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import Index from './index';
+
+it('renders correctly', () => {
+  const wrapper = shallow(<Index />);
+  const wrapper2 = shallow(
+    <Index
+      env={{
+        REACT_APP_BUILD_VERSION: '1',
+        REACT_APP_BUILD_COMMIT: '2',
+        REACT_APP_BUILD_ENV: '3',
+        REACT_APP_BUILD_BRANCH: '4',
+      }}
+    />,
+  );
+
+  expect(toJson(wrapper)).toMatchSnapshot('without env');
+  expect(toJson(wrapper2)).toMatchSnapshot('with env');
+});
