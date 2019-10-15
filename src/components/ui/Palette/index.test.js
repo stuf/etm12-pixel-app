beba3beeba6a3cc8f0bd495211a3298cfdd5d821
Stefan Rimaila <stefan@rimaila.fi>
2019-10-15 13:53:21 +0300

    test(components): improve tests for Palette component

--- src/components/ui/Palette/index.test.js
+++ src/components/ui/Palette/index.test.js
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme';
 
 import Palette from './index';
 
-it('renders without crashing with minimal data', () => {
+it('renders without crashing', () => {
   shallow(<Palette items={[]} />);
+
+  shallow(<Palette items={['#f00', '#00f']} currentColor={0} />);
 });
