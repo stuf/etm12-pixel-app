79d3ae59b1f204f9ebeb284def4610d27e039796
Stefan Rimaila <stefan@rimaila.fi>
2019-10-16 19:25:33 +0300

    chore: update typedefs

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,6 +2,34 @@ import { Observable, Property } from 'kefir';
 import { IState, ICanvasState } from 'core/state';
 import { IMenuItem } from 'core/models';
 
+//
+
+/**
+ * Unary function
+ */
+export interface Ary1Fn<A, R> {
+  (a: A): R;
+}
+
+/**
+ * Curried binary function
+ */
+export interface Ary2Fn<A, B, R> {
+  (a: A): (b: B) => R;
+  (a: A, b: B): R;
+}
+
+/**
+ * Curried ternary function
+ */
+export interface Ary3Fn<A, B, C, R> {
+  (a: A): Ary2Fn<B, C, R>;
+  (a: A, b: B): (c: C) => R;
+  (a: A, b: B, c: C): R;
+}
+
+//
+
 export interface BuildCommitEnv {
   REACT_APP_BUILD_BRANCH: string;
   REACT_APP_BUILD_COMMIT: string;
@@ -16,6 +44,14 @@ export interface RootProps {
   env: BuildCommitEnv & { [key: string]: string };
 }
 
+//
+
 export type Value<T> = Property<T, any>;
 
 export type MaybeObservable<T> = T | Observable<T, any>;
+
+//
+
+export interface LiftedAry1Fn<A, R> {
+  (a: MaybeObservable<A>): MaybeObservable<R>;
+}
