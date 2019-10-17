cec942ae62ef665c3d4a393348393662124b2822
Stefan Rimaila <stefan@rimaila.fi>
2019-10-17 13:40:01 +0300

    feat(common): update meta models

--- src/common/meta.test.js
+++ src/common/meta.test.js
@@ -1,9 +1,14 @@
 import { testEq } from 'test-utils';
+import * as L from 'partial.lenses';
 
 import * as M from './meta';
 
 testEq('foo', () => M.nameIn({ name: 'foo' }));
 testEq('bar', () => M.currentIn({ current: 'bar' }));
+testEq(false, () => M.isEditingIn({ isEditing: false }));
 testEq(['top'], () => M.itemsIn({ items: ['top'] }));
 testEq([], () => M.itemsIn({ items: [] }));
 testEq([], () => M.itemsIn());
+
+testEq(123, () => L.get(M.fmt.rNumber, '123'));
+testEq(123, () => L.set(M.fmt.wNumber, '123', undefined));
