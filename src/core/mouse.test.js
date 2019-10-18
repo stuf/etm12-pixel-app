ead12e490b1c720b7bf961f25435c5140785ff7b
Stefan Rimaila <stefan@rimaila.fi>
2019-10-18 20:59:45 +0300

    test(core): mouse: add mouseEventsFor tests

---
+++ src/core/mouse.test.js
@@ -0,0 +1,35 @@
+import { mouseEventsFor } from './mouse';
+
+let dom;
+let fn;
+let ev;
+
+beforeEach(() => {
+  dom = document.createElement('div');
+  fn = jest.fn();
+  ev = mouseEventsFor(dom);
+});
+
+it('onButtonDown', done => {
+  ev.onButtonDown.onValue(fn);
+
+  ev.onButtonDown.take(1).onEnd(() => {
+    expect(fn).toHaveBeenCalled();
+    done();
+  });
+
+  dom.dispatchEvent(new MouseEvent('mousedown'));
+});
+
+it('onDrag', done => {
+  ev.onDrag.onValue(fn);
+
+  ev.onDrag.take(1).onEnd(() => {
+    expect(fn).toHaveBeenCalled();
+    done();
+  });
+
+  dom.dispatchEvent(new MouseEvent('mousedown'));
+  dom.dispatchEvent(new MouseEvent('mousemove'));
+  dom.dispatchEvent(new MouseEvent('mouseup'));
+});
