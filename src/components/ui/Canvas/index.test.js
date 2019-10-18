b21a8430c31870f2c05a2de3d1904fb47031d916
Stefan Rimaila <stefan@rimaila.fi>
2019-10-19 01:14:48 +0300

    test(components): canvas: take updating canvas data into account as well

--- src/components/ui/Canvas/index.test.js
+++ src/components/ui/Canvas/index.test.js
@@ -1,37 +1,46 @@
 import * as React from 'karet';
+import * as K from 'kefir';
 import * as U from 'karet.util';
-import * as H from 'kefir.partial.lenses.history';
+import * as L from 'kefir.partial.lenses';
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import jestKefir from 'jest-kefir';
 
 import Index from './index';
 import originalState from 'core/state';
 
+const { activate, extensions, value } = jestKefir(K);
+expect.extend(extensions);
+
 let state;
+let canvasData;
 
 beforeEach(() => {
   state = U.atom(originalState.get());
   state
     .view(['currentFile', 'createdAt'])
     .set(new Date('2019-10-18T21:24:25.594Z'));
+
+  canvasData = U.atom([]);
 });
 
 it('works shallowly', () => {
-  const canvasData = U.atom([]);
   const { canvas, color } = U.destructure(state);
   const { size, scale } = U.destructure(canvas);
 
-  const wrap = shallow(<Index {...{ size, scale, color, canvasData }} />);
-
-  expect(toJson(wrap)).toMatchSnapshot('shallow snapshot');
+  const wrap = shallow(
+    <Index {...{ size, scale, color, canvasData: U.atom([]) }} />,
+  );
 });
 
 it('works less shallowly', () => {
-  const canvasData = U.atom(H.init({}, []));
   const { canvas, color } = U.destructure(state);
   const { size, scale } = U.destructure(canvas);
 
   const wrap = mount(<Index {...{ size, scale, color, canvasData }} />);
 
+  U.view(L.slice(0, 4), canvasData).set([255, 255, 0, 127]);
+
+  expect(canvasData.get().slice(0, 4)).toEqual([255, 255, 0, 127]);
   expect(toJson(wrap)).toMatchSnapshot('deep snapshot');
 });
