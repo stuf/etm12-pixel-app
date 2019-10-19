14c1893f8db8cc7781c42f52d3cf861b0018f78d
Stefan Rimaila <stefan@rimaila.fi>
2019-10-19 22:49:40 +0300

    feat(common): canvas: add getContext function

--- src/common/canvas.js
+++ src/common/canvas.js
@@ -1,8 +1,13 @@
 import * as U from 'karet.util';
 
-import { LiftedAry2Fn, Size } from 'types.d';
+import { LiftedAry1Fn, LiftedAry2Fn, Size } from 'types.d';
 
 /**
  * @type {LiftedAry2Fn<Size, number, Size>}
  */
 export const scaleSize = U.lift(([w, h], m) => [w * m, h * m]);
+
+/**
+ * @type {LiftedAry1Fn<HTMLCanvasElement, CanvasRenderingContext2D>}
+ */
+export const getContext = U.lift(el => el.getContext('2d'));
