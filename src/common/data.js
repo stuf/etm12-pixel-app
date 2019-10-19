c0f57f9f1ff6442cb0ac784be80d5a3ec0e5b10f
Stefan Rimaila <stefan@rimaila.fi>
2019-10-19 22:47:53 +0300

    feat(common): data: add rangeScaled with tests

--- src/common/data.js
+++ src/common/data.js
@@ -1,11 +1,33 @@
 /**
  * @module data
  * @namespace common
- *
- * Common curried data structure constructors
  */
-import * as R from 'ramda';
+import * as U from 'karet.util';
+import * as R from 'kefir.ramda';
 
+import { LiftedAry1Fn, LiftedAry3Fn } from 'types.d';
+
+// Data structures
+
+/**
+ * @type {LiftedAry1Fn<number[], Uint8ClampedArray>}
+ */
 export const mkClampedArray = R.constructN(1, Uint8ClampedArray);
 
+/**
+ * @type {LiftedAry3Fn<Uint8ClampedArray, number, number, ImageData>}
+ */
 export const mkImageData = R.constructN(3, ImageData);
+
+// Utility functions
+
+/**
+ * @type {LiftedAry3Fn<number, number, number, number[]>}
+ */
+export const rangeScaled = U.lift((start, end, step) =>
+  U.combine([start, end, step], (a, b, s) =>
+    Array(((b - a) / s + 1))
+      .fill(a)
+      .map((_, i) => a + i * s),
+  ),
+);
--- src/common/data.test.js
+++ src/common/data.test.js
@@ -1,7 +1,8 @@
-import * as Mod from './data';
+import * as D from './data';
+import { testEq } from 'test-utils';
 
 it('mkClampedArray', () => {
-  const xs = Mod.mkClampedArray([1, 2, 3, 4, 5, 6, 7, 8]);
+  const xs = D.mkClampedArray([1, 2, 3, 4, 5, 6, 7, 8]);
 
   expect(xs).toBeInstanceOf(Uint8ClampedArray);
 });
@@ -10,8 +11,14 @@ it('mkImageData', () => {
   const w = 5;
   const h = 5;
   const m = 4;
-  const xs = Mod.mkClampedArray(Array(w * h * m).fill(0));
+  const xs = D.mkClampedArray(Array(w * h * m).fill(0));
 
-  const ctor = () => Mod.mkImageData(xs, w, h);
+  const ctor = () => D.mkImageData(xs, w, h);
   expect(ctor).not.toThrow();
 });
+
+//
+
+testEq([0, 1, 2, 3, 4, 5], () => D.rangeScaled(0, 5, 1));
+testEq([0, 5, 10, 15, 20, 25], () => D.rangeScaled(0, 25, 5));
+testEq([10, 20, 30, 40, 50], () => D.rangeScaled(10, 50, 10));
