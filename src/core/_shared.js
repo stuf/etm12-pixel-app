f50e7feb2c7e2ac9907f265c5221929b678d6009
Stefan Rimaila <stefan@rimaila.fi>
2019-10-19 19:17:48 +0300

    chore(core): add linter rules to ignore typedefs

--- src/core/_shared.js
+++ src/core/_shared.js
@@ -1,10 +1,13 @@
+/* eslint no-unused-vars: [1, {"varsIgnorePattern": "[Value]"}] */
 import * as U from 'karet.util';
+
+import { Value } from 'types.d';
 
 /**
  * @template T
  * @param {string} type
  * @param {T.MaybeObservable<HTMLElement, any>} source
- * @return {K.Property<T, any>}
+ * @return {Value<T>}
  */
 export const takeEvents = (type, source) =>
   U.thru(
