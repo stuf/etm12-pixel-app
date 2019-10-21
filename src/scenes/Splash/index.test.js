99340585025b3b06a401d1da3e06b08056318dab
Stefan Rimaila <stefan@rimaila.fi>
2019-10-21 19:07:14 +0300

    test(scenes): splash: add test for redirectDelay

--- src/scenes/Splash/index.test.js
+++ src/scenes/Splash/index.test.js
@@ -1,11 +1,25 @@
 import * as React from 'karet';
-import { shallow } from 'enzyme';
+import * as K from 'kefir';
+import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-import Index from './index';
+import SplashScene from './index';
 
 it('renders correctly', () => {
-  const wrapper = shallow(<Index />);
+  const wrapper = shallow(<SplashScene />);
 
   expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+it('triggers configured redirect', done => {
+  const history = {
+    replace: jest.fn(),
+  };
+
+  const wrapper = mount(<SplashScene history={history} redirectDelay={0} />);
+
+  K.later(50, null).onValue(() => {
+    expect(history.replace).toHaveBeenCalled();
+    done();
+  });
 });
