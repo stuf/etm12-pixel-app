a0e7524544d076bc5bd456042b7380f016ea8251
Stefan Rimaila <stefan@rimaila.fi>
2019-10-21 14:07:30 +0300

    feat(components): canvas: remove deprecated canvas

--- src/components/ui/Canvas/_/PixelGrid/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { FunctionComponent } from 'react';
-
-export interface Props {
-  size: [number, number];
-  scale: number;
-}
-
-export interface Component extends FunctionComponent<Props> { }
