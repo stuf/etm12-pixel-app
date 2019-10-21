bef2f69a79be063c0368dac1e6e2bb2fc392683d
Stefan Rimaila <stefan@rimaila.fi>
2019-10-21 23:53:19 +0300

    feat(scenes): editor: use error case for editor if no canvasData

--- src/scenes/Editor/index.js
+++ src/scenes/Editor/index.js
@@ -4,7 +4,7 @@
  * @namespace scenes
  */
 import * as React from 'karet';
-import * as K from 'kefir';
+import * as A from 'kefir.atom';
 import * as U from 'karet.util';
 import * as L from 'kefir.partial.lenses';
 import * as Z from 'kefir.partial.lenses.history';
@@ -28,7 +28,6 @@ import { empty } from 'common/canvas';
 
 /**
  * @param {T.Props} props
- * @return {T.Component}
  */
 function EditorScene(props) {
   const { state, canvasData, menuItems, env } = props;
@@ -42,107 +41,111 @@ function EditorScene(props) {
   const selectedPalette = U.view(currentPalette, palettes);
   const selectedColor = U.view(currentColor, M.itemsIn(selectedPalette));
 
+  const imageDataValid = canvasData instanceof A.AbstractMutable;
+
   const ensureEmptyImage = U.thru(
     empty(size),
-    U.consume(data => {
-      if (!(canvasData instanceof K.Property)) {
-        console.warn('`canvasData` is not observable!', canvasData);
-        console.trace();
-
-        U.view(Z.present, canvasData).set(data);
-      }
-    }),
+    U.consume(data => U.view(Z.present, canvasData).set(data)),
   );
 
   return (
     <div className={U.cns('scene-root', styles.root)} data-scene-name="editor">
-      <>{U.sink(ensureEmptyImage)}</>
-      <LayoutHeader
-        {...{
-          env,
-          menuItems,
-          className: styles.top,
-          name: M.nameIn(currentFile),
-          isEditing: M.isEditingIn(currentFile),
-        }}
-      />
-
-      <div className={U.cns(styles.left, styles.panel)}>
-        <Group title="Palette">
-          <Dropdown {...{ items: palettes, value: currentPalette }} />
-          <Palette
+      {U.ifElse(
+        imageDataValid,
+        <>
+          <>{U.sink(ensureEmptyImage)}</>
+          <LayoutHeader
             {...{
-              currentColor,
-              name: U.view('name', selectedPalette),
-              items: U.view('items', selectedPalette),
-            }}
-          />
-        </Group>
-      </div>
-
-      <div className="relative-pos">
-        <Canvas
-          {...{
-            size,
-            scale,
-            drawable,
-            data,
-            color: selectedColor,
-          }}
-        />
-      </div>
-
-      <div className={U.cns(styles.right, styles.panel)}>
-        <Group title="Preview">
-          <Bitmap
-            {...{
-              size,
-              scale: 2,
-              data: U.view(Z.present, canvasData),
-            }}
-          />
-        </Group>
-
-        <Group title="Image">
-          <Field
-            label="Name"
-            value={U.view(['currentFile', 'name', L.valueOr('')], state)}
-          />
-
-          <Button>Clear image</Button>
-        </Group>
-
-        <Group title="History">
-          <div>{U.mapValue(x => x - 1, U.view(Z.count, canvasData))}</div>
-
-          <Range
-            {...{
-              value: U.view(Z.index, canvasData),
-              min: 0,
-              max: Z.indexMax(canvasData),
-              onChange: U.getProps({
-                valueAsNumber: U.view(Z.index, canvasData),
-              }),
+              env,
+              menuItems,
+              className: styles.top,
+              name: M.nameIn(currentFile),
+              isEditing: M.isEditingIn(currentFile),
             }}
           />
 
-          <TimeControlButton count={U.view(Z.undoIndex, canvasData)}>
-            Undo
-          </TimeControlButton>
-          <TimeControlButton count={U.view(Z.redoIndex, canvasData)}>
-            Redo
-          </TimeControlButton>
-          <Button group onClick={U.doModify(canvasData, Z.undoForget)}>
-            Purge history
-          </Button>
-        </Group>
+          <div className={U.cns(styles.left, styles.panel)}>
+            <Group title="Palette">
+              <Dropdown {...{ items: palettes, value: currentPalette }} />
+              <Palette
+                {...{
+                  currentColor,
+                  name: U.view('name', selectedPalette),
+                  items: U.view('items', selectedPalette),
+                }}
+              />
+            </Group>
+          </div>
 
-        <Group title="Canvas">
-          <Field label="Width" value={U.view([0, M.fmt.wNumber], size)} />
-          <Field label="Height" value={U.view([1, M.fmt.wNumber], size)} />
-          <Field label="Scale" value={U.view(M.fmt.wNumber, scale)} />
-        </Group>
-      </div>
+          <div className="relative-pos">
+            <Canvas
+              {...{
+                size,
+                scale,
+                drawable,
+                data,
+                color: selectedColor,
+              }}
+            />
+          </div>
+
+          <div className={U.cns(styles.right, styles.panel)}>
+            <Group title="Preview">
+              <Bitmap
+                {...{
+                  size,
+                  scale: 2,
+                  data: U.view(Z.present, canvasData),
+                }}
+              />
+            </Group>
+
+            <Group title="Image">
+              <Field
+                label="Name"
+                value={U.view(['currentFile', 'name', L.valueOr('')], state)}
+              />
+
+              <Button>Clear image</Button>
+            </Group>
+
+            <Group title="History">
+              <div>{U.mapValue(x => x - 1, U.view(Z.count, canvasData))}</div>
+
+              <Range
+                {...{
+                  value: U.view(Z.index, canvasData),
+                  min: 0,
+                  max: Z.indexMax(canvasData),
+                  onChange: U.getProps({
+                    valueAsNumber: U.view(Z.index, canvasData),
+                  }),
+                }}
+              />
+
+              <div>
+                <TimeControlButton count={U.view(Z.undoIndex, canvasData)}>
+                  Undo
+                </TimeControlButton>
+                <TimeControlButton count={U.view(Z.redoIndex, canvasData)}>
+                  Redo
+                </TimeControlButton>
+              </div>
+
+              <Button onClick={U.doModify(canvasData, Z.undoForget)}>
+                Purge history
+              </Button>
+            </Group>
+
+            <Group title="Canvas">
+              <Field label="Width" value={U.view([0, M.fmt.wNumber], size)} />
+              <Field label="Height" value={U.view([1, M.fmt.wNumber], size)} />
+              <Field label="Scale" value={U.view(M.fmt.wNumber, scale)} />
+            </Group>
+          </div>
+        </>,
+        <div>somethings wrong</div>,
+      )}
     </div>
   );
 }
--- src/scenes/Editor/index.test.js
+++ src/scenes/Editor/index.test.js
@@ -7,12 +7,16 @@ import toJson from 'enzyme-to-json';
 import Index from './index';
 
 it('matches snapshot', () => {
-  const state = {
+  const state = U.atom({
     canvas: { size: [32, 32], scale: 16 },
     color: { currentPalette: 0, currentColor: 0, palettes: [] },
-  };
+  });
 
   const canvasData = U.atom(H.init({ maxCount: 10, replacePeriod: 200 }, []));
 
-  const w = shallow(<Index {...{ state, canvasData }} />);
+  const w = mount(<Index {...{ state, canvasData }} />);
+
+  const w2 = mount(<Index {...{ state, canvasData: canvasData.get() }} />);
+
+  state.view('size').set([16, 16]);
 });
