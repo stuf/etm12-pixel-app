8436605c791e70b49989c4842d823d1dd22be56e
Stefan Rimaila <stefan@rimaila.fi>
2019-10-23 22:22:06 +0300

    chore: housekeeping

--- src/core/effects.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @module effects.d
- * @namespace core
- *
- * Application-wide side-effects (commands)
- */
--- src/core/mouse.d.ts
+++
@@ -1,5 +0,0 @@
-import { Observable } from 'kefir';
-
-export type MaybeObservable<T> = T | Observable<T, any>;
-
-export interface Ev { }
