b9706b9856ae6ee12d12191b9419bb9d6c8cc21a
Stefan Rimaila <stefan@rimaila.fi>
2019-10-26 00:35:49 +0300

    chore(components): panel: housekeeping

--- src/components/layout/Panel.js
+++ src/components/layout/Panel.js
@@ -1,4 +1,5 @@
 import * as React from 'karet';
+import * as K from 'kefir';
 import * as U from 'karet.util';
 import * as R from 'kefir.ramda';
 import * as L from 'kefir.partial.lenses';
@@ -7,17 +8,13 @@ const C = R.always;
 const eq = R.equals;
 
 export default function Panel({ direction = 'vertical', size, children }) {
-  const style = {};
-
   const size$ = U.thru(
     U.combine([direction, size], (d, s) => ({
       [R.ifElse(eq('vertical'), C('height'), C('width'))(d)]: s,
     })),
   );
 
-  if (size) {
-    style[direction === 'vertical' ? 'height' : 'width'] = size;
-  }
+  const style = U.thru(U.template([size$]), R.mergeAll);
 
   const childs = React.Children.toArray(children);
   const childSizes = L.collectTotal([L.elems, 'props', 'size'], childs);
