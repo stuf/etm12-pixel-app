9f31161756e17cfad8f68a5e8498ce6e53c72319
Stefan Rimaila <stefan@rimaila.fi>
2019-10-27 18:15:02 +0200

    feat(core): effects: add wip effects handler

--- src/core/__tests__/effects.js
+++ src/core/__tests__/effects.js
@@ -1,7 +1,13 @@
-import { saveImageFromCanvas, effValue, dispatchEff } from 'core/effects';
+import * as K from 'kefir';
+import {
+  saveImageFromCanvas,
+  handlers,
+  mkHandler,
+  effValue,
+  dispatchEff,
+  registerHandler,
+} from 'core/effects';
 import { Property } from 'kefir';
-
-import { testEq } from 'test-utils';
 
 jest.mock('file-saver');
 
@@ -14,7 +20,39 @@ describe('effects', () => {
     }).not.toThrow();
   });
 
-  it('effValue :: Property', () => {
+  it('effValue', () => {
     expect(effValue).toBeInstanceOf(Property);
+  });
+
+  it('dispatchEff', done => {
+    effValue.take(1).onValue(eff => {
+      expect(eff).toEqual({ type: 'foo' });
+      done();
+    });
+
+    dispatchEff({ type: 'foo' });
+  });
+
+  it('allows registering effects handlers', done => {
+    handlers
+      .skip(4)
+      .take(1)
+      .onValue(v => {
+        expect(v).toMatchObject({
+          something: [{}, {}],
+          'something-else': [{}],
+        });
+        done();
+      });
+
+    const h1 = mkHandler('something', a => a);
+    const h2 = mkHandler('something-else', b => b);
+    const h1b = mkHandler('something', a => a);
+
+    registerHandler(h1);
+    registerHandler(h2);
+    registerHandler(h1);
+    registerHandler(h1b);
+    registerHandler(h1);
   });
 });
--- src/core/effects.js
+++ src/core/effects.js
@@ -7,6 +7,8 @@
  */
 import * as V from 'kefir.partial.lenses.validation';
 import * as U from 'karet.util';
+import * as L from 'kefir.partial.lenses';
+import * as K from 'kefir';
 import { saveAs } from 'file-saver';
 
 /**
@@ -34,3 +36,56 @@ const effBus = U.bus();
 export const effValue = U.toProperty(effBus);
 
 export const dispatchEff = eff => effBus.push(eff);
+
+//
+
+/**
+ * @type {K.Stream<Handler, any>}
+ */
+const handlerBus = U.bus();
+
+export const mkHandler = (type, fn, help) => ({ type, fn, help });
+
+/**
+ * @type {K.Property<Object.<string, Handler[]>>}
+ */
+export const handlers = handlerBus
+  .scan(
+    (handlers, handler) =>
+      L.set(
+        [handler.type, L.valueOr([]), L.find(x => x.fn === handler.fn)],
+        handler,
+        handlers,
+      ),
+    {},
+  )
+  .toProperty();
+
+export const registerHandler = handler => {
+  handlerBus.push(handler);
+};
+
+//
+
+/**
+ *
+ * @param {K.Property<any, any>} handlerSource
+ */
+export const handleEffsWith = (handlerSource, effSource) => {
+  const effDispatch = K.combine([effSource], [handlerSource]);
+};
+
+//
+
+/**
+ * @typedef {object} Eff
+ * @prop {string} type
+ * @prop {any} [payload]
+ */
+
+/**
+ * @typedef {object} Handler
+ * @prop {string} type
+ * @prop {Function} fn
+ * @prop {string} [help]
+ */
