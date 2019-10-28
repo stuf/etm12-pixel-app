0d4c276caa4d2ab2521abf85c57287e3d48446cd
Stefan Rimaila <stefan@rimaila.fi>
2019-10-28 13:30:08 +0200

    feat(core): effects: add base effects implementation with validation

--- src/core/__tests__/effects.js
+++ src/core/__tests__/effects.js
@@ -1,4 +1,6 @@
-import { saveImageFromCanvas } from 'core/effects';
+import { is } from 'ramda';
+import { combine, constant } from 'kefir';
+import { saveImageFromCanvas, effValidated, pushEff } from 'core/effects';
 
 jest.mock('file-saver');
 
@@ -9,5 +11,25 @@ describe('effects', () => {
     expect(() => {
       saveImageFromCanvas(canvas);
     }).not.toThrow();
+  });
+
+  it('effValidated', done => {
+    const values = effValidated.take(1);
+    const errors = effValidated.takeErrors(1).flatMapErrors(constant);
+
+    combine([values, errors])
+      .flatMap(constant)
+      .observe({
+        value: ([vs, es]) => {
+          expect(is(Object, vs)).toBe(true);
+          expect(is(Object, es)).toBe(true);
+        },
+        end: () => {
+          done();
+        },
+      });
+
+    pushEff({ type: 'lul', dorp: 'derp' });
+    pushEff({ type: 'some-effect', payload: { some: 'data' } });
   });
 });
--- src/core/effects.js
+++ src/core/effects.js
@@ -5,7 +5,14 @@
  *
  * @todo This needs some thinking about how this should be reasoned around
  */
+import * as K from 'kefir';
+import * as R from 'kefir.ramda';
+import * as U from 'karet.util';
+import * as V from 'kefir.partial.lenses.validation';
+
 import { saveAs } from 'file-saver';
+
+// Legacy
 
 /**
  * @param {HTMLCanvasElement} canvas
@@ -13,3 +20,32 @@ import { saveAs } from 'file-saver';
 export const saveImageFromCanvas = canvas => {
   canvas.toBlob(b => saveAs(b, 'poopoo.png'));
 };
+
+// Validation
+
+const ruleEff = V.propsOr(V.rejectAs('unexpected field'), {
+  type: [R.is(String), 'required string'],
+  payload: V.optional(V.accept),
+});
+
+// Effect bus
+
+const effBus = U.bus();
+
+const effValue = U.toProperty(effBus);
+
+export const effValidated = U.flatMapLatest(eff => {
+  const errors = V.errors(ruleEff, eff);
+
+  if (errors) {
+    return K.constantError(errors);
+  }
+
+  return K.constant(eff);
+}, effValue);
+
+export const pushEff = effBus.push.bind(effBus);
+
+//
+
+const handleEffs = U.thru(effValidated);
