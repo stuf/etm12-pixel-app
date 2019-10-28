2b4ba4e05f4f5ec549a853af6f723490f8fed675
Stefan Rimaila <stefan@rimaila.fi>
2019-10-28 17:06:48 +0200

    feat(components): ui: remove redundant Details and Menu components

--- src/components/ui/Details/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { ReactElement, FunctionComponent } from 'react';
-
-export interface Props {
-  title: string;
-  children: ReactElement[];
-  open: boolean;
-}
-
-export interface Component extends FunctionComponent<Props> { }
--- src/components/ui/Details/index.test.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'karet';
-import { shallow } from 'enzyme';
-
-import Details from './index';
-
-it('renders without crashing', () => {
-  shallow(<Details title="some details">some content</Details>);
-});
