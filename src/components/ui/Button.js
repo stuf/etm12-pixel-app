e5562d5854d6422f6c12c3f73518caa4ae3b146b
Stefan Rimaila <stefan@rimaila.fi>
2019-10-28 17:45:21 +0200

    feat(components): button: add support for id

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -2,10 +2,11 @@
 import * as React from 'karet';
 import * as U from 'karet.util';
 
-function Button({ children, disabled, action, group }) {
+function Button({ id, children, disabled, action, group }) {
   return (
     <button
       {...{
+        id,
         className: U.cns('button', U.when(group, '--group')),
         disabled,
         onClick: U.actions(U.preventDefault, action),
