cf3a34d04aab769e854856d1b1b99f265052bc99
Stefan Rimaila <stefan@rimaila.fi>
2019-10-29 16:36:30 +0200

    feat(core): state
    add devtools flags

--- src/components/molecules/panel/Devtool.js
+++ src/components/molecules/panel/Devtool.js
@@ -1,13 +1,22 @@
 import * as React from 'karet';
 import * as U from 'karet.util';
+import * as L from 'kefir.partial.lenses';
 
 import { Checkbox } from 'components/form';
 
 export default function Devtool({ flags }) {
   const offsetGuidesIn = U.view('offsetGuide');
+
+  const keys = L.collect(L.keys, flags);
+
   return (
     <section>
-      <Checkbox label="Offset guides" value={offsetGuidesIn(flags)} />
+      {U.thru(
+        keys,
+        U.mapElems((it, i) => (
+          <Checkbox key={`flag-${i}`} label={it} value={U.view(it, flags)} />
+        )),
+      )}
     </section>
   );
 }
