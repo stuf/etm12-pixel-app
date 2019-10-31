d78672a88a140af6d8474bcb5ed363054b4b6cf1
Stefan Rimaila <stefan@rimaila.fi>
2019-11-01 00:48:05 +0200

    test(core-components): canvas: improve base tests for canvas

--- src/components/core/__tests__/Canvas.js
+++ src/components/core/__tests__/Canvas.js
@@ -1,16 +1,29 @@
 import * as React from 'karet';
 import * as U from 'karet.util';
+import * as H from 'kefir.partial.lenses.history';
 import { shallow, mount } from 'enzyme';
+import state from 'core/state';
+import canvasData from 'core/canvas-data';
+import { itemsIn } from 'common/meta';
 
 import Canvas from 'components/core/Canvas';
 
 it('renders correctly', () => {
-  const state = U.atom({
-    size: [32, 32],
-    scale: 16,
-  });
+  // @todo Rewrite state-dependent tests to be less hacky
+  const { canvas, color, devtool } = U.destructure(state);
+  const { size, scale } = U.destructure(canvas);
+  const { currentColor, currentPalette, palettes } = U.destructure(color);
+  const selectedPalette = U.view(currentPalette, palettes);
+  const selectedColor = U.view(currentColor, itemsIn(selectedPalette));
 
-  const { size, scale, color } = U.destructure(state);
-
-  const wrap = shallow(<Canvas {...{ size, scale, color }} />);
+  const wrap = mount(
+    <Canvas
+      {...{
+        size,
+        scale,
+        data: U.view(H.present, canvasData),
+        color: selectedColor,
+      }}
+    />,
+  );
 });
