88266b88fbb0392acf51c1e23f0430a69d9a6243
Stefan Rimaila <stefan@rimaila.fi>
2019-10-31 11:49:23 +0200

    test(scenes): load-image: minor test improvements

--- src/scenes/LoadImage.js
+++ src/scenes/LoadImage.js
@@ -3,10 +3,11 @@ import * as L from 'kefir.partial.lenses';
 import * as U from 'karet.util';
 import * as R from 'kefir.ramda';
 import * as K from 'kefir';
-import * as V from 'kefir.partial.lenses.validation';
-import * as H from 'kefir.partial.lenses.history';
 import * as FR from 'karet.fr';
+import { present } from 'kefir.partial.lenses.history';
 
+import { mkElem } from 'common/dom';
+import { getContext, getImageData } from 'common/canvas';
 import { fstIn, sndIn } from 'common/meta';
 
 import Panel from 'components/layout/Panel';
@@ -14,6 +15,11 @@ import Button from 'components/ui/Button';
 import Modal from 'components/ui/Modal';
 
 export default function LoadImageScene(props) {
+  const actions = U.serializer(null);
+
+  const { state, canvasData, history } = props;
+  const canvasSize = U.view(['canvas', 'size'], state);
+
   const loadImage = U.flatMapLatest(dataURL => {
     const img = new Image();
     img.src = dataURL;
@@ -51,22 +57,63 @@ export default function LoadImageScene(props) {
 
   const loadedImage = U.thru(dataURL, U.flatMapLatest(loadImage), U.toProperty);
   const imageSize = U.mapValue(R.props(['width', 'height']), loadedImage);
-
-  const width = fstIn(imageSize);
-  const height = sndIn(imageSize);
   const src = L.get('src', loadedImage);
+
+  //
+
+  const canvas = U.mapValue(
+    ([w, h]) => mkElem('canvas', { width: w, height: h }),
+    imageSize,
+  );
+
+  const ctx = getContext(canvas);
+  const id = getImageData(ctx, imageSize);
+
+  //
+
+  const putImageEff = U.thru(
+    U.template([ctx, loadedImage]),
+    U.consume(([c, i]) => c.drawImage(i, 0, 0)),
+  );
+
+  //
+
+  const loadImageEff = U.doPush(actions, () =>
+    U.thru(
+      id,
+      U.takeFirst(1),
+      U.consume(({ data, width, height }) => {
+        U.holding(() => {
+          canvasSize.set([width, height]);
+          canvasData.view(present).set(data);
+          history.push('/editor');
+        });
+      }),
+    ),
+  );
+
+  //
 
   return (
     <section className={U.cns('scene-root', 'load-image-root')}>
+      <>{U.sink(U.parallel([putImageEff, actions]))}</>
       <Panel>
         <Modal
           header="Load image"
-          footer={<Button disabled={R.not(hasImage)}>Load image</Button>}
+          footer={
+            <Button
+              id="load-image"
+              action={loadImageEff}
+              disabled={R.not(hasImage)}
+            >
+              Load image
+            </Button>
+          }
         >
           <input
             type="file"
             accept="image/png"
-            onChange={U.actions(U.getProps({ files }))}
+            onChange={U.getProps({ files })}
           />
 
           <hr />
@@ -84,7 +131,7 @@ export default function LoadImageScene(props) {
                     {fstIn(imageSize)} × {sndIn(imageSize)} pixels
                   </div>
                   <div className="loadImage__previewImage">
-                    <img {...{ src, width, height }} />
+                    <img {...{ src }} />
                   </div>
                 </div>
               </section>,
