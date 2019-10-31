f75c95ab19e38b64cc3940a5c8dd739c46e1ff09
Stefan Rimaila <stefan@rimaila.fi>
2019-11-01 00:31:58 +0200

    build(test-setup): add global ResizeObserver polyfill for now

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import 'jest-canvas-mock';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import ResizeObserver from 'resize-observer-polyfill';
+
+global.ResizeObserver = ResizeObserver;
 
 configure({ adapter: new Adapter() });
